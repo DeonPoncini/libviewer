435733263ef1b2bf44ca8a0d89648244f810180a
Deon Poncini <dex1337@gmail.com>
2014-11-26 17:15:04 -0800

    Fix to work with Ubuntu 14.04 mesa OpenGL 3.3

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -15,7 +15,7 @@ Scene::Scene(int width, int height, const char* title) :
     }
 
     glfwWindowHint(GLFW_SAMPLES, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
